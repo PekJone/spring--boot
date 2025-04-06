cd089dec1a2d2c875485cc3c36167f6c697f0916
Murphy <742662307@qq.com>
2025-04-06 10:17:00 +0800

    添加mybatis启动器

--- Springboot3/springboot3-01-first/src/main/java/org/com/MyApplication.java
+++ Springboot3/springboot3-01-first/src/main/java/org/com/MyApplication.java
@@ -1,6 +1,7 @@
 package org.com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -8,11 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @date ${YEAR}-${MONTH}-${DAY}  ${TIME}
  */
+
 @SpringBootApplication
 public class MyApplication {
 
     public static void main(String[] args) {
         /**
+         * @EnableAutoConfiguration 注解 启用自动配置  默认自动启用自动配置
+         * 自动配置：
+         *     所谓的自动配置，sprongboot应用会去类路径当中查找class  根据路径中有哪些类 来自动管理bean
          * run方法的第一个参数实际就是一个配置类 对应以前 配置文件
          *
          */
